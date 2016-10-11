ea007487a951161a692cfa99a122c92d36c2fea5
chitrareddy <chitrareddy47@gmail.com>
2016-10-10 22:52:39 -0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,14 @@
+
+
+
+	class Node
+	{
+		double eval,gain;
+		String feature;
+		 int clas_count[]=new int[]{0,0,0,0,0};
+		double ds[][];
+		int rows,colno;
+		String child;
+		Node left,right;
+		
+	}
